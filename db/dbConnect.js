bf7a73754268341264ed0da175e77ee1495b6179
Prabin Kumar Mahato <prabinmahato414@gmail.com>
2023-10-01 03:16:24 +0545

    up

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 
 export const dataBase = ()=>{
     const db_url = process.env.MONGODB_URL;
-    mongoose.connect(db_url, {
-        dbName:process.env.DB_NAME
-    }
+    mongoose.connect(db_url
     ).then(()=>{
         console.log("db Connected");
     }).catch((e)=>{
