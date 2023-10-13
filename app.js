684cefe733ff98ef896dea46c23f04cd3249c8af
Prabin Kumar Mahato <prabinmahato414@gmail.com>
2023-10-14 03:46:17 +0545

    update backedn

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ import { dataBase } from "./db/dbConnect.js";
 import userRouter from './routes/route.js'
 import cors from 'cors';
 import bodyParser from "body-parser";
-import multer from "multer";
+import multer, { diskStorage } from "multer";
 import { User } from "./schema/user.js";
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+
+const storage = multer.diskStorage({
+    destination: function(cb, req, file){
+        cb(null, '/public');
+    },
+    filename: function(cb, req,file){
+        cb(null, Date.now()+'_'+ file.originalname)
+    }
+});
+
+const upload = multer({storage:storage});
+
+app.post('/api/:id/upload', upload.single('image') ,(req, res)=>{
+    res.json({ file: req.file });
+})
 
 dataBase();
 
